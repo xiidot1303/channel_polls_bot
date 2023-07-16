2710335c68e18a63b573b1ed3fc99fb6e3df452b
xiidot1303 <xii1303@inbox.ru>
2023-07-16 18:55:52 +0500

    add path in bot config

--- bot/apps.py
+++ bot/apps.py
@@ -1,9 +1,11 @@
 from django.apps import AppConfig
 import os
+from django.conf import settings
 
 class bot(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bot'
+    path = os.path.join(settings.BASE_DIR, 'bot')
     def ready(self):
         run_once = os.environ.get('CMDLINERUNNER_RUN_ONCE_BOT')
         if run_once is not None:
