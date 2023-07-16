f8369a454e38eea20b1f94918c842640374aef66
xiidot1303 <xii1303@inbox.ru>
2023-07-16 18:56:58 +0500

    remove scheduler to bot

--- bot/apps.py
+++ bot/apps.py
@@ -6,10 +6,10 @@ class bot(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bot'
     path = os.path.join(settings.BASE_DIR, 'bot')
-    def ready(self):
-        run_once = os.environ.get('CMDLINERUNNER_RUN_ONCE_BOT')
-        if run_once is not None:
-            return
-        os.environ['CMDLINERUNNER_RUN_ONCE_BOT'] = 'True'
-        from bot.scheduled_job.updater import jobs
-        jobs.scheduler.start()
+    # def ready(self):
+    #     run_once = os.environ.get('CMDLINERUNNER_RUN_ONCE_BOT')
+    #     if run_once is not None:
+    #         return
+    #     os.environ['CMDLINERUNNER_RUN_ONCE_BOT'] = 'True'
+    #     from bot.scheduled_job.updater import jobs
+    #     jobs.scheduler.start()
